9e9521f55c6b72d3c3da7aeb9379fc37059b3e5b
georgi-gahov <georgi.gahov@gmail.com>
2022-11-24 11:39:42 +0200

    OTHERS

---
+++ JavaScript/MERN-Tut_1/frontend/src/CreatePost.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const CreatePost = () => {
+
+    const [post, setPost] = useState({
+        title: "",
+        description: ""
+    });
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setPost((prev) => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    };
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        axios.post("/create", post)
+            .then((res) => console.log(res))
+            .then((err) => console.log(err));
+        navigate("posts");
+    }
+
+    return (
+        <div style={{ width: "90%", margin: "auto auto", textAlign: "center" }}>
+            <h1>Create Post</h1>
+            <Form>
+                <Form.Group>
+                    <Form.Control
+                        name="title"
+                        placeholder='Title'
+                        value={post.title}
+                        onChange={handleChange}
+                        style={{ marginBottom: "1rem" }} />
+                    <Form.Control
+                        name="description"
+                        placeholder='Description'
+                        value={post.description}
+                        onChange={handleChange}
+                        style={{ marginBottom: "1rem" }} />
+                </Form.Group>
+                <Button
+                    variant='outline-success'
+                    style={{ width: "100%", marginBottom: "1rem" }}
+                    onClick={handleClick}>CREATE POST</Button>
+            </Form>
+            <Button
+                variant='outline-dark'
+                style={{ width: "100%" }}
+                onClick={() => navigate(-1)}>BACK</Button>
+        </div>
+    )
+};
+
+export default CreatePost;
---
+++ JavaScript/MERN-Tut_1/server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.static('public'));
+
+// mongoose.connect(process.env.DB_CONNECT, () => {
+//     console.log("Connect to DB");
+// });
+mongoose.connect(process.env.DB_CONNECT)
+    .then(() => {
+        console.log("Connected to DB")
+    })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+    })
+    .catch((err) => console.log(err));
+
+const postSchema = mongoose.Schema({
+    title: {
+        type: String
+    },
+    description: {
+        type: String
+    }
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get('/', (req, res) => {
+    res.send("Express here")
+});
+
+app.post('/create', (req, res) => {
+    // console.log(req.body)
+    Post.create({
+        title: req.body.title,
+        description: req.body.description
+    })
+        // .then(doc => console.log(doc))
+        .catch((err) => {
+            console.log(err)
+        })
+});
+
+app.get("/posts", (req, res) => {
+    Post.find().then(items => res.json(items))
+        .catch((err) => console.log(err));
+});
+
+app.delete("/delete/:id", (req, res) => {
+    Post.findByIdAndDelete({ _id: req.params.id })
+        // .then(doc => console.log(doc))
+        .catch((err) => console.log(err));
+});
+
+app.put("/update/:id", (req, res) => {
+    // console.log(req.params);
+    // console.log(req.body);
+    Post.findByIdAndUpdate({ _id: req.params.id }, {
+        title: req.body.title,
+        description: req.body.description
+    })
+        // .then(doc => console.log(doc))
+        .catch((err) => console.log(err));
+});
+
+// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
