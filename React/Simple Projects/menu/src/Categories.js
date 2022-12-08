1c5b4308cb7860071008ca5dc5dd2453e60dbc8c
georgi-gahov <georgi.gahov@gmail.com>
2022-12-08 12:22:28 +0200

    OTHERS

---
+++ React/Simple Projects/menu/src/Categories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Categories = ({ filterItems, categories }) => {
+    return (
+        <div className='btn-container'>
+            {
+                categories[0].map((category, index) => {
+                    return (
+                        <button type='button' className='filter-btn' key={index} onClick={() => filterItems(category)}>{category}</button>
+                    )
+                })
+            }
+        </div>
+    )
+};
+
+export default Categories;
